38db3cb510240989128336df63d50314d8fa9b12
Amol Jagtap <amoljagtap17@gmail.com>
2023-03-10 11:26:13 +0530

    2023-react-rtk-query-demo: add redux toolkit

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,9 @@ import "@fontsource/montserrat";
 import CssBaseline from "@mui/material/CssBaseline";
 import { ThemeProvider } from "@mui/material/styles";
 import { theme } from "app/mui";
+import { store } from "app/store";
 import { router } from "components/routes";
+import { Provider } from "react-redux";
 import { RouterProvider } from "react-router-dom";
 
 export function App() {
@@ -10,7 +12,9 @@ export function App() {
     <ThemeProvider theme={theme}>
       <CssBaseline />
 
-      <RouterProvider router={router} />
+      <Provider store={store}>
+        <RouterProvider router={router} />
+      </Provider>
     </ThemeProvider>
   );
 }
